be3719be7a46e341532c5cc1de0cf8f9cd4d2091
RujiraKongsomran <rujirabiwberry@gmail.com>
2022-02-06 16:40:46 +0700

    Bottom Sheet Material Dialog

--- app/src/main/java/com/rujirakongsomran/materialdialoganimation/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/materialdialoganimation/MainActivity.java
@@ -3,8 +3,10 @@ package com.rujirakongsomran.materialdialoganimation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import dev.shreyaspatil.MaterialDialog.AbstractDialog;
+import dev.shreyaspatil.MaterialDialog.BottomSheetMaterialDialog;
 import dev.shreyaspatil.MaterialDialog.MaterialDialog;
 import dev.shreyaspatil.MaterialDialog.interfaces.DialogInterface;
 
@@ -15,26 +17,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MaterialDialog mDialog = new MaterialDialog.Builder(this)
+//        MaterialDialog mDialog = new MaterialDialog.Builder(this)
+//                .setAnimation(R.raw.delete)
+//                .setTitle("Delete?")
+//                .setMessage("Are you sure want to delete this file?")
+//                .setCancelable(false)
+//                .setPositiveButton("Delete", R.drawable.ic_delete, new AbstractDialog.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int which) {
+//
+//                    }
+//                })
+//                .setNegativeButton("Cancel", R.drawable.ic_close, new AbstractDialog.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int which) {
+//                        dialogInterface.dismiss();
+//                    }
+//                })
+//                .build();
+//
+//        // Show Dialog
+//        mDialog.show();
+        BottomSheetMaterialDialog mBottomSheetDialog = new BottomSheetMaterialDialog.Builder(this)
                 .setAnimation(R.raw.delete)
                 .setTitle("Delete?")
                 .setMessage("Are you sure want to delete this file?")
                 .setCancelable(false)
-                .setPositiveButton("Delete", R.drawable.ic_delete, new AbstractDialog.OnClickListener() {
+                .setPositiveButton("Delete", R.drawable.ic_delete, new BottomSheetMaterialDialog.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int which) {
-
+                        Toast.makeText(getApplicationContext(), "Deleted!", Toast.LENGTH_SHORT).show();
+                        dialogInterface.dismiss();
                     }
                 })
-                .setNegativeButton("Cancel", R.drawable.ic_close, new AbstractDialog.OnClickListener() {
+                .setNegativeButton("Cancel", R.drawable.ic_close, new BottomSheetMaterialDialog.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int which) {
+                        Toast.makeText(getApplicationContext(), "Cancelled!", Toast.LENGTH_SHORT).show();
                         dialogInterface.dismiss();
                     }
                 })
                 .build();
 
         // Show Dialog
-        mDialog.show();
+        mBottomSheetDialog.show();
     }
 }
